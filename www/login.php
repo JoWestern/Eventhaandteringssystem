7f5100ceba5167c232832776e65b6faacf82726c
ErlendDev90 <erlend.lotsberg@gmail.com>
2022-10-10 14:47:26 +0200

    Noenlunde brukende login-side

--- www/login.php
+++ www/login.php
@@ -1,8 +1,22 @@
-<h1>Logg inn</h1>
+<?php include "./assets/inc/header.php" ?>
+    <body class="text-center">
+        <div class="container login" style="width: fit-content">
+            <main class="form-signin w-100 m-auto">
+                <form>
+                    <br>
+                    <h1 class="h3 mb-3 fw-normal">Logg inn</h1>
 
-<form>
-    <input type="email" name="email" placeholder="E-post"><br>
-    <input type="password" name="password" placeholder="Passord"><br><br>
-    <input type="submit">
-</form>
-<?php
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="E-post">
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Passord">
+                    </div>
+
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    <p class="mt-5 mb-3 text-muted">© 2022</p>
+                </form>
+            </main>
+        </div>
+    </body>
+<?php ?>
