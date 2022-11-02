f35eb9822319a72e00f026275862152916809394
ErlendDev90 <erlend.lotsberg@egde.no>
2022-11-02 12:56:16 +0100

    Laget kort

--- www/assets/lib/class.Display.php
+++ www/assets/lib/class.Display.php
@@ -2,38 +2,43 @@
     class Display{
         function displayCards($result){
             if ($result->num_rows > 0) {
-                //table headers
-                echo "<table>
-                            <tr>
-                                <th>ID</th>
-                                <th>Tittel</th>
-                                <th>Informasjon</th>
-                                <th>Vert</th>
-                                <th>Sted</th>
-                                <th>Start</th>
-                                <th>Slutt</th>
-                                <th>Kategori</th>
-                                <th>Pris</th>
-                                <th>Nettside</th>
-                            </tr>";
+                echo "<div class=\"flex-container\">";
                 
                 //echo each row from table
                 while($row = $result->fetch_assoc()) {
-                    echo
-                    "<tr>
-                    <td>" . $row["event_id"] . "</td>
-                    <td>" . $row["title"] . "</td>
-                    <td>" . $row["info"] . "</td>
-                    <td>" . $row["first_name"] . " " . $row["last_name"] . "</td>
-                    <td>" . $row["location"] . "</td>
-                    <td>" . $row["time"] . "</td>
-                    <td>" . $row["endtime"] . "</td>
-                    <td>" . $row["name"] . "</td>
-                    <td>" . $row["ticketprice"] . "</td>
-                    <td>" . $row["website"] . "</td>
-                    </tr>";
+                    echo "
+                    <div class=\"card\" style=\"width: 18rem;\">
+                    <div class=\"card-img\">
+                        <img class=\"card-img-top\" src=\"...\" alt=\"Card image cap\">
+                    </div>
+                    <div class=\"card-body d-flex flex-column\">
+                        <h5 class=\"card-title\">" . $row["title"] . "</h5>
+                        <p class=\"card-text\">" . $row["info"] . "</p>
+                    </div>
+                    <div class=\"card-footer\">
+                        <a href=\"\" class\"btn btn-primary\">Les mer</a>
+                    </div>
+                    </div>
+                    ";
+
+
+
+
+
+                    // "<tr>
+                    // <td>" . $row["event_id"] . "</td>
+                    // <td>" . $row["title"] . "</td>
+                    // <td>" . $row["info"] . "</td>
+                    // <td>" . $row["first_name"] . " " . $row["last_name"] . "</td>
+                    // <td>" . $row["location"] . "</td>
+                    // <td>" . $row["time"] . "</td>
+                    // <td>" . $row["endtime"] . "</td>
+                    // <td>" . $row["name"] . "</td>
+                    // <td>" . $row["ticketprice"] . "</td>
+                    // <td>" . $row["website"] . "</td>
+                    // </tr>";
                 }
-                echo "</table>";
+                echo "</div>";
                 }
                 else {
                 echo "0 results";
