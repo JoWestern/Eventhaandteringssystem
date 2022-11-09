f5f7648356193e660d6cf66b3d0e48b767d74db6
ErlendDev90 <erlend.lotsberg@egde.no>
2022-11-09 15:56:53 +0100

    Bilder&link fungerer, endret funksjon for display

--- www/event.php
+++ www/event.php
@@ -1,4 +1,14 @@
 <?php 
 require dirname(__DIR__)."/www/assets/inc/header.php";
+require dirname(__DIR__)."/www/assets/lib/class.Event.php";
 ?>
-<h1>Vis 1 event</h1>
+
+<?php
+$event_id = $_REQUEST['event_id'];
+
+$event = new Event();
+$result = $event->singleEvent($event_id);
+$thisEvent = $result->fetch_object();
+
+echo "<h1>" . $thisEvent->title . "</h1>";
+?>
