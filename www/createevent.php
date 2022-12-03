04765758eef3ad62431a869ab760092fd66675e2
Joachim Western <joachimwestern@Joachims-MacBook-Pro.local>
2022-12-03 12:34:59 +0100

    la til ferdig bileopplastning med placeholder - la til main tag i register.php

--- www/createevent.php
+++ www/createevent.php
@@ -8,69 +8,76 @@ require __DIR__."/assets/inc/authenticate.php";
 <html>
     <body class="main text-left">
         <div class="container login mt-5" style="width: fit-content">
-        <div class="">
+        <div class="img-fluid">
         <?php 
             // $img = "assets/img/stock.png";
             // function showPic($filename){
-                if (isset($_POST["show"])){
+            if (isset($_POST["show"])){
                 $src = checkFile();
-                // echo "<div>
-                // <img id='showPic' src=\"" . $img . "\" alt=\"Arrangementsbilde\" width='600rem'>
-                // </div>";
-                echo "<img id='showPic' src=\"" . $src . "\" alt=\"Arrangementsbilde\" width='600rem'>";
-            } 
+                displayImage($src);
+            } else {
+                $src = "assets/img/stock.png";
+                displayImage($src);
+            }
         ?>
         </div>
             <form method="POST" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> autocomplete="off" enctype="multipart/form-data">
                 <h1 class="h3 mb-3 fw-normal">Opprett arrangement</h1>
-                <label for="title">Tittel:</label>
-                <div class="form-floating">
-                    <input type="text" id="title" name="title" autocomplete="off">
-                </div>
-                <label for="bio">Beskrivelse:</label>
-                <div class="form-floating">
-                    <input type="text" id="bio" name="bio" autocomplete="off">
-                </div>
-                <label for="local">Lokasjon:</label>
-                <div class="form-floating">
-                    <input type="text" id="local" name="local" autocomplete="off">
-                </div>
-                <label for="startdate">Startdato:</label>
-                <div class="form-floating">
-                    <input type="datetime-local" id="startdate" name="startdate" autocomplete="off">
-                </div>
-                <label for="enddate">Sluttdato:</label>
-                <div class="form-floating">
-                    <input type="datetime-local" id="enddate" name="enddate" autocomplete="off">
-                </div>
-                <label for="ticketprice">Pris:</label>
+                    <label for="title">Tittel:</label>
                     <div class="form-floating">
-                        <input type="number" id="ticketprice" name="ticketprice" autocomplete="off">
+                        <input class="form-control form-control-sm" type="text" id="title" name="title" autocomplete="off">
                     </div>
-                <label class="mt-1" for="cat">Kategori:</label>
-                <div class="form-floating">
-                    <select name="category">
-                        <?php
-                            $category = new Category();
-                            $categoryOptions = $category->selectCategory();
-                            while ($row = mysqli_fetch_array($categoryOptions)) {
-                                echo "<option value='" . $row['category_id'] . "'>" . $row['name'] . "</option>";
-                            }
-                        ?>
-                    </select>
-                </div>
-                <label for="website">Link til nettside:</label>
-                <div class="form-floating">
-                    <input type="text" id="website" name="website" autocomplete="off">
-                </div>
-                <div class="form-floating">
-                    <label for="imgFile">Velg fil:</label>
+                    <label for="bio">Beskrivelse:</label>
                     <div class="form-floating">
-                        <input name="imgFile" type="file">
+                        <input class="form-control form-control-sm" type="text" id="bio" name="bio" autocomplete="off">
                     </div>
-                    <input name="show" type="submit" value="Vis bilde">
+                    <label for="local">Lokasjon:</label>
+                    <div class="form-floating">
+                        <input class="form-control form-control-sm" type="text" id="local" name="local" autocomplete="off">
+                    </div>
+                    <label for="startdate">Startdato:</label>
+                    <div class="form-floating">
+                        <input class="form-control form-control-sm" type="datetime-local" id="startdate" name="startdate" autocomplete="off">
+                    </div>
+                    <label for="enddate">Sluttdato:</label>
+                    <div class="form-floating">
+                        <input class="form-control form-control-sm" type="datetime-local" id="enddate" name="enddate" autocomplete="off">
+                    </div>
+                    <label for="ticketprice">Pris:</label>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">kr</span>
+                        </div>
+                        <div class="form-floating">
+                            <input class="form-control" type="number" id="ticketprice" name="ticketprice" autocomplete="off">
+                        </div>
+                    </div>
+                    <label class="mt-1" for="cat">Kategori:</label>
+                    <div class="form-floating">
+                        <select class="form-control form-control-sm" name="category">
+                            <?php
+                                $category = new Category();
+                                $categoryOptions = $category->selectCategory();
+                                while ($row = mysqli_fetch_array($categoryOptions)) {
+                                    echo "<option value='" . $row['category_id'] . "'>" . $row['name'] . "</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <label for="website">Link til nettside:</label>
+                    <div class="form-floating">
+                        <input class="form-control form-control-sm" type="text" id="website" name="website" autocomplete="off" placeholder="www.nettside.no">
+                    </div>
+                    <div class="form-group mb-3">
+                        <div class="form-floating" style="margin:5px;">
+                            <label for="imgFile">Legg til bilde:</label>
+                            <div>
+                                <input name="imgFile" type="file">
+                                <input name="show" type="submit" value="Lagre bilde">
+                            </div>
+                        </div>
+                    <input class="w-100 btn btn-lg btn-primary mt-3" type="submit" name="submit" value="Opprett" autocomplete="off"></input>
                 </div>
-                <input class="w-100 btn btn-lg btn-primary mt-3" type="submit" name="submit" value="Opprett" autocomplete="off"></input>
             </form>
         </div>
     </body>
@@ -97,13 +104,11 @@ if (isset($_POST["submit"])) {
 
     $events = new Event();
     $CreateEvent = $events->createEvent($title, $info, $host, $location, $starttime, $cat, $endtime, $ticketprice, $website);
-    echo "<p class='mb-3 fw-normal'>Arrangementet er lagret!</p>";
+    echo "<p class='mb-3 fw-normal'>Arrangementet er oppretter!</p>";
 }
 
 //runs when form has been submitted
 function checkFile() {
-    // if (isset($_POST["show"])){
-
     // Define array for messages 
     $messages = array();
     
@@ -119,8 +124,7 @@ function checkFile() {
                                 "png" => "image/png"
         );
         $max_file_size = 2097152; // in bytes
-        // $dir = $_SERVER['DOCUMENT_ROOT'] . "/Eventhaandteringssystem/www/assets/img/";
-        $dir = "/Applications/XAMPP/xamppfiles/htdocs/Eventhaandteringssystem/www/assets/img/";
+        $dir = $_SERVER['DOCUMENT_ROOT'] . "/Eventhaandteringssystem/www/assets/img/";
         // No directory with that name?
         if(!file_exists($dir)) 
         {
@@ -130,8 +134,6 @@ function checkFile() {
         
         // Constructing file name
         $suffix = array_search($_FILES['imgFile']['type'], $acc_file_types);
-
-        //$filename  = "gunnar" . '.' . $suffix;
 
         /* If the file already exists for some reason */
         do {
@@ -165,41 +167,24 @@ function checkFile() {
             $uploaded_file = move_uploaded_file($_FILES['imgFile']['tmp_name'], $filepath);
            
             if ($uploaded_file){
-                $img = "assets/img/".$filename;
-                // showPic($img);
-                //echo "<img class='' src=\"" . $img . "\" alt=\"Arrangementsbilde\" width='600rem'>";
                 $messages['success'][] = "Filen ble lastet opp!";
+                $img = "assets/img/" . $filename;
+                return $img;
             } else {
                 $messages['error'][] = "Not uploaded because of error #".$_FILES["imgFile"]["error"];
+                $img = "assets/img/stock.png";
+                return $img;
             }
-            $img = "assets/img/" . $filename;
-            return $img;
-            // if (!$uploaded_file) 
-            //     $messages['error'][] = "Filen kunne ikke lastes opp";
-            // else
-            //     $messages['success'][] = "Filen ble lastet opp!";
         }
 
     } else {
         $messages['error'][] = "Ingen fil er lastet opp";
-        $img = "assets/img/stock.png";
+        $img = "/stock.png";
         return $img;
     }
-    // Output messages to user
-    // if(isset($messages) && !empty($messages))
-    // {
-    //     echo "<strong>Message" . (sizeof($messages, COUNT_RECURSIVE)-1 > 1 ? "s:<br>" : ":<br>") . "</strong>";
-    //     foreach($messages as $msg_type => $type_messages)
-    //     {
-    //         if($msg_type == 'error')
-    //             foreach($type_messages as $message) { echo '<span style="color:red";>- ' . $message . '</span><br>'; }
-    //         elseif($msg_type == 'success')
-    //             foreach($type_messages as $message) { echo '<span style="color:green";>- ' . $message . '</span><br>'; }
-    //     }
-    // }
-    // else
-    // {
-    //     echo "<strong>Vennligst velg fil for opplasting</strong>";
-    // }
+}
+
+function displayImage($src) {
+    echo "<img class='placeholderImg' id='showPic' src='" . $src . "' alt=\"Arrangementsbilde\" style=\"width: 20rem; margin: 150px;\">";
 }
 ?>
